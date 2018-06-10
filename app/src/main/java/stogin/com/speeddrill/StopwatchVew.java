3419143dde5b5b2144adb1c79fc98dbcc262d837
William Stogin <William@William-PC.(none)>
2018-06-09 21:10:53 -0500

    Updated stopwatch to not change size everytime the number changes

--- app/src/main/java/stogin/com/speeddrill/StopwatchVew.java
+++ app/src/main/java/stogin/com/speeddrill/StopwatchVew.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -12,8 +14,9 @@ public class StopwatchVew extends View {
     private Paint paint;
     private String text;
     private Rect bounds;
-    private float verticalPadding = 10;
-    private float horizontalPadding = 10;
+    private Handler mHandler;
+    long startTime;
+    float textSize;
 
     public StopwatchVew(Context context, AttributeSet attributeSet) {
         super(context, attributeSet);
@@ -24,16 +27,16 @@ public class StopwatchVew extends View {
         paint.setStyle(Paint.Style.FILL);
         paint.setAntiAlias(true);
 
-        this.setPadding(10,10,100,10);
+        this.setPadding(10,10,10,10);
 
-        text = "0:00:00";
+        text = "0:00:000";
+        textSize = determineTextSize();
+
+        mHandler = new Handler();
+        start();
     }
 
-    @Override
-    protected void onDraw(Canvas canvas) {
-
-
-
+    private float determineTextSize() {
         final float testTextSize = 48f;
 
         // Get the bounds of the text, using our testTextSize.
@@ -43,17 +46,45 @@ public class StopwatchVew extends View {
         // Calculate the desired size as a proportion of our testTextSize.
         float fullWidthSize = testTextSize * (this.getMeasuredWidth() - this.getPaddingRight() - this.getPaddingLeft()) / bounds.width();
         float fullHeightSize = testTextSize * (this.getMeasuredHeight() - this.getPaddingBottom() - this.getPaddingTop()) / bounds.height();
+        return Math.min(fullHeightSize, fullWidthSize);
+    }
 
-        paint.setTextSize(Math.min(fullHeightSize, fullWidthSize));
+
+    @Override
+    protected void onSizeChanged(int xNew, int yNew, int xOld, int yOld) {
+        super.onSizeChanged(xNew, yNew, xOld, yOld);
+        textSize = determineTextSize();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        paint.setTextSize(textSize);
 
         canvas.drawText(text, this.getPaddingLeft(), this.getMeasuredHeight() - getPaddingBottom(), paint);
     }
 
     public void start() {
-
+        startTime = SystemClock.uptimeMillis();
+        mHandler.postDelayed(update, 0);
     }
 
     public void pause() {
 
     }
+
+    private Runnable update = new Runnable() {
+        @Override
+        public void run() {
+            long currentTime = SystemClock.uptimeMillis();
+            long diff = currentTime - startTime;
+
+            String minutes = String.format("%01d", (int) diff / 60000);
+            diff = diff - 60000*((int) diff/60000);
+            String seconds = String.format("%02d", (int) (diff / 1000));
+            String millis = String.format("%03d",(int) (diff % 1000));
+            text = minutes + ":" + seconds + ":" + millis;
+            StopwatchVew.this.invalidate();
+            mHandler.postDelayed(update, 50);
+        }
+    };
 }
